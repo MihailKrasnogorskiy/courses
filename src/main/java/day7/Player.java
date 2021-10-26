8b0822c9e5fd5c20824c1721bf2d8bd322dd290a
Mihail Krasnogorskiy <mikser256@gmail.com>
2021-10-26 16:05:14 +0300

    complete day7 tasks

---
+++ src/main/java/day7/Player.java
@@ -0,0 +1,48 @@
+package day7;
+
+public class Player {
+    public Player(int stamina) {
+        this.stamina = stamina;
+        countPlayers++;
+        if (countPlayers > 6) {
+            countPlayers = 6;
+        }
+    }
+
+    public int stamina;
+    static final int MAX_STAMINA = 100;
+    static final int MIN_STAMINA = 0;
+    private static int countPlayers;
+
+    public static int getCountPlayers() {
+        return countPlayers;
+    }
+
+    public void run() {
+
+        if (this.stamina == 1) {
+            countPlayers--;
+        }
+        if (this.stamina == MIN_STAMINA) {
+            return;
+        }
+        this.stamina -= 1;
+    }
+
+    public int getStamina() {
+        return stamina;
+    }
+
+    public static void info() {
+        String infoText = "Команды не полные. На поле есть " + (6 - countPlayers);
+        if (countPlayers == 6) {
+            System.out.println("На поле нет свободных мест");
+        } else if (countPlayers == 5) {
+            System.out.println(infoText + "свободное место");
+        } else if (countPlayers >= 2 && countPlayers <= 4) {
+            System.out.println(infoText + "свободных места");
+        } else if (countPlayers == 1 || countPlayers == 0) {
+            System.out.println(infoText + "свободных мест");
+        }
+    }
+}
--- src/main/java/day7/Task2.java
+++ src/main/java/day7/Task2.java
@@ -1,7 +1,29 @@
 package day7;
 
+import java.util.Random;
+
 public class Task2 {
     public static void main(String[] args) {
-
+        Random rand = new Random();
+        Player player1 = new Player(100 -rand.nextInt(11));
+        System.out.println(player1.getStamina());
+//        Player.info();
+        Player player2 = new Player(100 -rand.nextInt(11));
+//        Player.info();
+        Player player3 = new Player(100 -rand.nextInt(11));
+//        Player.info();
+        Player player4 = new Player(100 -rand.nextInt(11));
+//        Player.info();
+        Player player5 = new Player(100 -rand.nextInt(11));
+//        Player.info();
+        Player player6 = new Player(100 -rand.nextInt(11));
+//        Player.info();
+        Player player7 = new Player(100 -rand.nextInt(11));
+//        Player.info();
+         for (int i=1; i<=101; i++) {
+             player1.run();
+             System.out.println(player1.getStamina());
+             Player.info();
+         }
     }
 }

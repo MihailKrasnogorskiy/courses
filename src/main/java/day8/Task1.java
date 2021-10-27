58e6ad3e211c9722ef926a03c56d5606ef6007a7
Mihail Krasnogorskiy <mikser256@gmail.com>
2021-10-27 08:25:39 +0300

    complete day8 tasks

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,24 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+        String s = "1";
+        long startTime = System.currentTimeMillis();
+        for (int i = 2; i <= 20000; i++) {
+            s = s + " " + i;
+        }
+        System.out.println(s);
+        long stopTime = System.currentTimeMillis();
+        System.out.println("Длительность работы, в мс.: " + (stopTime - startTime)); //396ms
 
+        StringBuilder sb = new StringBuilder();
+        sb.append(1);
+        startTime = System.currentTimeMillis();
+        for (int i = 2; i <= 20000; i++) {
+            sb.append(" ");
+            sb.append(i);
+        }
+        System.out.println(sb);
+        stopTime = System.currentTimeMillis();
+        System.out.println("Длительность работы, в мс.: " + (stopTime - startTime)); //17ms
     }
 }

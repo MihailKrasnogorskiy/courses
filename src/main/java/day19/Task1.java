12f41720a221b374ab17faa34d0c66693cb6b894
Mihail Krasnogorskiy <mikser256@gmail.com>
2021-11-11 06:52:17 +0300

    day18 in developing

--- src/main/java/day19/Task1.java
+++ src/main/java/day19/Task1.java
@@ -1,10 +1,39 @@
 package day19;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
 /**
  * @author Neil Alishev
  */
 public class Task1 {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws FileNotFoundException {
+        Map<String, Integer> wordsMap = new HashMap<>();
+        File text = new File("src/main/resources/dushi.txt");
+        Scanner scanner = new Scanner(text);
+        scanner.useDelimiter("[.,…:«;»„()?!\"\\s–]+");
+        while (scanner.hasNext()) {
+            String word = scanner.next();
+            word = word.toLowerCase(Locale.ROOT);
+            int a = 1;
+            if (wordsMap.containsKey(word)) {
+                a = wordsMap.get(word);
+                a++;
+                wordsMap.put(word, a);
+            } else wordsMap.put(word, a);
+        }
+        scanner.close();
+        List<Map.Entry<String, Integer>> wordsList = new ArrayList<>(wordsMap.entrySet());
+        Collections.sort(wordsList, new Comparator<Map.Entry<String, Integer>>() {
+            @Override
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                return o1.getValue().compareTo(o2.getValue());
+            }
+        });
+        for (int i = wordsList.size() - 1; i > wordsList.size() - 100; i--) {
+            System.out.println(wordsList.get(i));
+        }
+        System.out.println(wordsMap.get("чичиков")); //Чичиков 604 раза
     }
 }
--- src/main/java/day19/task3/Task3.java
+++ src/main/java/day19/task3/Task3.java
@@ -1,11 +1,28 @@
 package day19.task3;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 /**
  * @author Neil Alishev
  */
 public class Task3 {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws FileNotFoundException {
+        Map<Integer, Point> mapTaxi = new HashMap<>();
+        File file = new File("src/main/resources/taxi_cars.txt");
+        Scanner scanner = new Scanner(file);
+        while (scanner.hasNext()) {
+            String line = scanner.nextLine();
+            String[] splitLine = line.split(" ");
+            int[] taxiCars = new int[splitLine.length];
+            for (int i = 0; i < splitLine.length; i++) {
+                taxiCars[i] = Integer.parseInt(splitLine[i]);
+            }
+            mapTaxi.put(taxiCars[0], new Point(taxiCars[1], taxiCars[2]));
+        }
     }
 }
 
